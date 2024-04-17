410cada0c1c51640379374b99b1f4d7aec69cc0b
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2024-04-17 20:37:36 +0530

    fix minor bug in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,18 @@ export default async function Home() {
     );
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-fit gap-8 flex-col items-center justify-center p-24">
       <h2 className="text-3xl font-medium">
         This is Your Home page
       </h2>
-      {session && (
-        <UserInfo
-          name={session.user?.name as string}
-          email={session.user?.email as string}
-        />
-      )}
+      <div>
+        {session && (
+          <UserInfo
+            name={session.user?.name as string}
+            email={session.user?.email as string}
+          />
+        )}
+      </div>
     </main>
   );
 }
