a1f3b734e43a309735c5fe068a085a12e5350e3f
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2024-04-19 17:57:59 +0530

    prisma db added for oauth session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,13 @@
 import { AuthOptions } from "next-auth";
 import Facebook from "next-auth/providers/facebook";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
