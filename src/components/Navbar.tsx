09fc726d5ee036e2b7c6156882294ee7d21d8e15
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2024-04-24 14:01:36 +0530

    bug fix in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
-import SearchIcon from "@/components/ui/SearchIcon";
+import { signOut, useSession } from "next-auth/react";
 import Button from "./ui/Button";
 import Link from "next/link";
-import MoreIcon from "./ui/MoreIcon";
 
 const Navbar = () => {
+  const { status } = useSession();
   return (
     <div className="flex text-blue-950 justify-between items-center py-3 px-3 md:px-12 max-w-[1080px] md:mx-auto">
       <div className="flex gap-6 items-center">
@@ -13,22 +13,36 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="flex gap-6">
-        <Link className="flex justify-center" href="/login">
+      {status === "authenticated" ? (
+        <div className="flex gap-6">
           <Button
             type="button"
-            name="Log in"
+            name="Log out"
             className=" hover:text-blue-500"
+            onClick={() => signOut()}
           />
-        </Link>
-        <Link href={"/signup"}>
-          <Button
-            type="button"
-            name="Get Started"
-            className=" bg-green-400  hover:bg-green-500 py-2.5 px-4 "
-          />
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div className="flex gap-6">
+          <Link
+            className="flex justify-center"
+            href="/login"
+          >
+            <Button
+              type="button"
+              name="Log in"
+              className=" hover:text-blue-500"
+            />
+          </Link>
+          <Link href={"/signup"}>
+            <Button
+              type="button"
+              name="Get Started"
+              className=" bg-green-400  hover:bg-green-500 py-2.5 px-4 "
+            />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
