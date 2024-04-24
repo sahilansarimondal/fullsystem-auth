bc6c7473fdbecafb6f2974a0620999e21ace9171
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2024-04-24 12:07:36 +0530

    changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,26 +9,23 @@ const Navbar = () => {
     <div className="flex text-blue-950 justify-between items-center py-3 px-3 md:px-12 max-w-[1080px] md:mx-auto">
       <div className="flex gap-6 items-center">
         <Link href="/">
-          <div className="text-3xl font-bold">LG</div>
+          <div className="text-3xl font-bold">LOG0</div>
         </Link>
-        <SearchIcon />
-        <div className="hidden md:flex hover:text-blue-500">
-          <Button type="button" name="Explore" />
-          <MoreIcon />
-        </div>
       </div>
 
       <div className="flex gap-6">
-        <Button
-          type="button"
-          name="Start free trial"
-          className=" bg-green-400  hover:bg-green-500 py-2.5 px-4 "
-        />
         <Link className="flex justify-center" href="/login">
           <Button
             type="button"
             name="Log in"
             className=" hover:text-blue-500"
+          />
+        </Link>
+        <Link href={"/signup"}>
+          <Button
+            type="button"
+            name="Get Started"
+            className=" bg-green-400  hover:bg-green-500 py-2.5 px-4 "
           />
         </Link>
       </div>
