9d40519e0c29c3ff1e4ad3222da21a2d8a377924
Scott Blodgett <sblodgett@oncologycarepartners.com>
2024-05-31 17:11:24 -0400

    Fix up the nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,59 @@
 "use client";
-import { signOut, useSession } from "next-auth/react";
+
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSession } from "next-auth/react";
 import Button from "./ui/Button";
 import Link from "next/link";
 
-const Navbar = () => {
-  const { status } = useSession();
-  return (
-    <div className="flex text-blue-950 justify-between items-center py-3 px-3 md:px-12 max-w-[1080px] md:mx-auto">
-      <div className="flex gap-6 items-center">
-        <Link href="/">
-          <div className="text-3xl font-bold">LOG0</div>
-        </Link>
-      </div>
+const NavBar = () => {
+  const { data: session, status } = useSession();
 
-      {status === "authenticated" ? (
-        <div className="flex gap-6">
-          <Button
-            type="button"
-            name="Log out"
-            className=" hover:text-blue-500"
-            onClick={() => signOut()}
-          />
-        </div>
-      ) : (
-        <div className="flex gap-6">
-          <Link
-            className="flex justify-center"
-            href="/login"
-          >
-            <Button
-              type="button"
-              name="Log in"
-              className=" hover:text-blue-500"
-            />
-          </Link>
-          <Link href={"/signup"}>
-            <Button
-              type="button"
-              name="Get Started"
-              className=" bg-green-400  hover:bg-green-500 py-2.5 px-4 "
-            />
-          </Link>
-        </div>
-      )}
-    </div>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <div className="text-3xl font-bold">LOGO</div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto d-flex align-items-center gap-6">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/query">Files</Nav.Link>
+            <Nav.Link href="/utils">Utils</Nav.Link>
+            <Nav.Link href="/admin">Admin</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="ms-auto d-flex align-items-center gap-6">
+            {status !== "unauthenticated" && (
+              <>
+                <Nav.Link href="/login">
+                  <Button type="button" name="Log in" className="hover:text-blue-500" />
+                </Nav.Link>
+                <Nav.Link href="/signup">
+                  <Button type="button" name="Get Started" className="bg-green-400 hover:bg-green-500 py-2.5 px-4" />
+                </Nav.Link>
+              </>
+            )}
+            {status !== "authenticated" && (
+              <Nav.Link href="#logout">
+                <Button type="button" name="Log out" className="hover:text-blue-500" onClick={() => signOut()} />
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavBar;
